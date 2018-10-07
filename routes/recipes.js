3c0f558071b0c119822cf37cc31870a1cab78481
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-10-07 12:36:56 +0530

    recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,3 +1,14 @@
 var express=require('express');
 var router=express.Router();
 var Recipe=require('../models/recipe');
+
+router.get('/recipes',function(req,res){
+    console.log(req.user);
+    Campground.find({},function(err,allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('campgrounds/index',{campgrounds:allCampgrounds,currentUser: req.user});
+        }
+    });
+});
