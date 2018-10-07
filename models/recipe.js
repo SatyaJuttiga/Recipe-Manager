1209f2edb2c2a17050199840636108aa103563fb
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-10-07 14:45:33 +0530

    new

--- models/recipe.js
+++ models/recipe.js
@@ -6,10 +6,10 @@ var recipeSchema=new mongoose.Schema({
     description:String,
 });
 var Recipe=module.exports=mongoose.model('Recipe',recipeSchema);
-
+/*
 Recipe.create({
     name:'Pasta',
     image:'https://images.media-allrecipes.com/images/56589.png',
     description:'Pasta is among the most popular Italian pastas. This easy-to-make recipe can be quickly prepared at home using some simple ingredients',
 });
-
+*/
