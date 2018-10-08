ff94e996f53814862c66275700535b78620cf477
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-10-08 12:09:28 +0530

    seed1

---
+++ routes/comments.js
@@ -0,0 +1,16 @@
+var express=require('express');
+var router=express.Router();
+var Recipe=require('../models/recipes');
+var Comment=require('../models/comment');
+
+/*
+router.get('/recipes/:id/comments/new',isLoggedin,function(req,res){
+    Recipe.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('comments/new',{campground:campground});
+        }
+    });
+});
+*/
