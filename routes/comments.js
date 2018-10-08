1a99d78ebdbdf26a2c6c52421fcd976965ec5caa
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-10-08 16:22:31 +0530

    modified

--- routes/comments.js
+++ routes/comments.js
@@ -3,15 +3,23 @@ var router=express.Router();
 var Recipe=require('../models/recipe');
 var Comment=require('../models/comment');
 
-/*
+
 router.get('/recipes/:id/comments/new',isLoggedin,function(req,res){
-    Recipe.findById(req.params.id,function(err,campground){
+    Recipe.findById(req.params.id,function(err,recipe){
         if(err){
             console.log(err);
         }else{
-            res.render('comments/new',{campground:campground});
+            res.render('comments/new',{recipe:recipe});
         }
     });
 });
-*/
+
+
+function isLoggedin(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
 module.exports=router;
