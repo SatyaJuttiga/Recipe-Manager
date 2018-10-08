ddf7be8998164583cc89b081a206ceea4de98a25
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-10-08 16:53:30 +0530

    commentsave

--- routes/comments.js
+++ routes/comments.js
@@ -14,6 +14,31 @@ router.get('/recipes/:id/comments/new',isLoggedin,function(req,res){
     });
 });
 
+router.post('/recipes/:id/comments',isLoggedin,function(req,res){
+    Recipe.findById(req.params.id,function(err,recipe){
+        if(err){
+            console.log(err);
+            res.redirect('/recipes');
+        }else{
+            Comment.create(req.body.comment,function(err,comment){
+                if(err){
+                    console.log(err);
+                    res.redirect('/recipes');
+                }else{
+                    Comment.create(req.body.comment,function(err,comment){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            recipe.comments.push(comment);
+                            recipe.save();
+                            res.redirect('/recipes/' + recipe._id);
+                        }
+                    })
+                }
+            });
+        }
+    });
+});
 
 function isLoggedin(req,res,next){
     if(req.isAuthenticated()){
